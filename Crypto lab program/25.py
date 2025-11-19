a5ab17288e35521aac25b9cb6228b0e7937123d6
aryanarun67 <aaru070306@gmail.com>
2025-11-19 23:06:59 +0530

    Update 25.py

--- Crypto lab program/25.py
+++ Crypto lab program/25.py
@@ -1,20 +1,40 @@
-# filename: 25_rsa_gcd_plaintext_factor.py
-"""
-If a plaintext block m shares nontrivial gcd with n, gcd(m,n) may reveal a factor of n.
-"""
-import math
+from math import gcd
 
-def gcd_factor(m,n):
-    g = math.gcd(m,n)
-    if 1 < g < n:
-        return g
-    return None
+def modinv(a, m):
+    def egcd(a, b):
+        if a == 0:
+            return b, 0, 1
+        g, y, x = egcd(b % a, a)
+        return g, x - (b // a) * y, y
+    g, x, _ = egcd(a, m)
+    if g != 1:
+        raise Exception('No modular inverse')
+    return x % m
 
-if __name__=='__main__':
-    n = int(input("n = ").strip())
-    m = int(input("known plaintext block m = ").strip())
-    g = gcd_factor(m,n)
-    if g:
-        print("Nontrivial factor found:", g)
-    else:
-        print("No nontrivial factor from gcd(m,n).")
+def rsa_common_factor_attack(n1, n2, e1, e2, c1, c2):
+    p = gcd(n1, n2)
+    if p == 1:
+        return None
+    q1 = n1 // p
+    q2 = n2 // p
+    phi1 = (p - 1) * (q1 - 1)
+    phi2 = (p - 1) * (q2 - 1)
+    d1 = modinv(e1, phi1)
+    d2 = modinv(e2, phi2)
+    m1 = pow(c1, d1, n1)
+    m2 = pow(c2, d2, n2)
+    return m1, m2
+
+n1 = 3233      # 61 * 53
+n2 = 3127      # 61 * 51
+e1 = 17
+e2 = 17
+c1 = pow(42, e1, n1)
+c2 = pow(42, e2, n2)
+
+m1, m2 = rsa_common_factor_attack(n1, n2, e1, e2, c1, c2)
+print("Recovered Message from n1:", m1)
+print("Recovered Message from n2:", m2)
+#output
+Recovered Message from n1: 42
+Recovered Message from n2: 42
