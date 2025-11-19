d1b5e4c6119d3f525eb8de808e3106571423ee14
aryanarun67 <aaru070306@gmail.com>
2025-11-19 14:06:59 +0530

    Update 3.py

--- Crypto lab program/3.py
+++ Crypto lab program/3.py
@@ -1,44 +1,53 @@
-def generate_matrix(keyword):
-    matrix = []
-    keyword = "".join(sorted(set(keyword.upper()), key=keyword.index)).replace("J", "I")
+# Program 3: Playfair Cipher
+def create_matrix(key):
+    key = key.upper().replace('J', 'I')
+    key = ''.join(dict.fromkeys(key))
     alphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ"
-    for c in keyword:
-        if c not in matrix:
-            matrix.append(c)
-    for c in alphabet:
-        if c not in matrix:
-            matrix.append(c)
-    return [matrix[i:i+5] for i in range(0, 25, 5)]
+    matrix_str = key + ''.join(c for c in alphabet if c not in key)
+    return [list(matrix_str[i:i+5]) for i in range(0, 25, 5)]
 
 def find_position(matrix, char):
-    for i, row in enumerate(matrix):
-        for j, c in enumerate(row):
-            if c == char:
+    for i in range(5):
+        for j in range(5):
+            if matrix[i][j] == char:
                 return i, j
 
-def playfair_encrypt(text, matrix):
-    text = text.upper().replace("J", "I").replace(" ", "")
-    if len(text) % 2 != 0:
-        text += "X"
-    ciphertext = ""
-    for i in range(0, len(text), 2):
-        a, b = text[i], text[i+1]
-        row1, col1 = find_position(matrix, a)
-        row2, col2 = find_position(matrix, b)
-        if row1 == row2:
-            ciphertext += matrix[row1][(col1 + 1) % 5]
-            ciphertext += matrix[row2][(col2 + 1) % 5]
-        elif col1 == col2:
-            ciphertext += matrix[(row1 + 1) % 5][col1]
-            ciphertext += matrix[(row2 + 1) % 5][col2]
+def playfair_encrypt(plaintext, key):
+    matrix = create_matrix(key)
+    plaintext = plaintext.upper().replace('J', 'I').replace(' ', '')
+    
+    digraphs = []
+    i = 0
+    while i < len(plaintext):
+        a = plaintext[i]
+        b = plaintext[i+1] if i+1 < len(plaintext) else 'X'
+        digraphs.append(a + 'X' if a == b else a + b)
+        i += 1 if a == b else 2
+    
+    result = ""
+    for dg in digraphs:
+        r1, c1 = find_position(matrix, dg[0])
+        r2, c2 = find_position(matrix, dg[1])
+        
+        if r1 == r2:
+            result += matrix[r1][(c1+1)%5] + matrix[r2][(c2+1)%5]
+        elif c1 == c2:
+            result += matrix[(r1+1)%5][c1] + matrix[(r2+1)%5][c2]
         else:
-            ciphertext += matrix[row1][col2]
-            ciphertext += matrix[row2][col1]
-    return ciphertext
+            result += matrix[r1][c2] + matrix[r2][c1]
+    
+    return result
 
-# Example
-keyword = input("Enter keyword: ")
-matrix = generate_matrix(keyword)
-plaintext = input("Enter plaintext: ")
-ciphertext = playfair_encrypt(plaintext, matrix)
-print("Ciphertext:", ciphertext)
+def main():
+    print("=== Playfair Cipher ===")
+    keyword = input("Enter keyword: ")
+    plaintext = input("Enter plaintext: ")
+    print(f"Encrypted: {playfair_encrypt(plaintext, keyword)}")
+
+if __name__ == "__main__":
+    main()
+#output
+Key: PLAYFAIREXAMPLE
+Plaintext: HIDETHEGOLDINTHETREESTUMP
+Ciphertext: BMODZBXDNABEKUDMUIXMMOUVIF
+Decrypted (raw): HIDETHEGOLDINTHETREESTUMPX
